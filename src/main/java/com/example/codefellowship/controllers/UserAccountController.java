2222f625b21ba342f9df29dad03624231b22c3a1
herndont <herndontrey@gmail.com>
2019-05-07 12:49:52 -0700

    not complete... but getting close to finishing lab16

---
+++ src/main/java/com/example/codefellowship/controllers/UserAccountController.java
@@ -0,0 +1,13 @@
+package com.example.codefellowship.controllers;
+
+import com.example.codefellowship.database.ApplicationUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserAccountController {
+    @Autowired
+    ApplicationUserRepository repo;
+
+    //Going to need to make more progress here for tomorrow
+}
