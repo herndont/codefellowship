8c5ce62391523492854c40723d680eb4b97b66c0
herndont <herndontrey@gmail.com>
2019-05-08 13:31:05 -0700

    Tried to complete all of the setup from yesterday. Still having trouble getting anything to work. Will work on this tonight and get farther along

--- src/main/java/com/example/codefellowship/controllers/UserAccountController.java
+++ src/main/java/com/example/codefellowship/controllers/UserAccountController.java
@@ -1,13 +1,61 @@
 package com.example.codefellowship.controllers;
 
+import com.example.codefellowship.database.ApplicationUser;
 import com.example.codefellowship.database.ApplicationUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
 
 @Controller
 public class UserAccountController {
+
     @Autowired
     ApplicationUserRepository repo;
 
-    //Going to need to make more progress here for tomorrow
+    @Autowired
+    PasswordEncoder encoder;
+
+    @GetMapping("/signup")
+    public String getSignup() {
+        return "signup";
+    }
+
+    @PostMapping("/signup")
+    public String postSignup(
+            @RequestParam String username,
+            @RequestParam String password,
+            @RequestParam String firstName,
+            @RequestParam String lastName,
+            @RequestParam Date dateOfBirth,
+            @RequestParam String bio
+    ){
+        ApplicationUser user = new ApplicationUser();
+        user.setUsername(username);
+        user.setPassword(encoder.encode(password));
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setDateOfBirth(dateOfBirth);
+        user.setBio(bio);
+
+        repo.save(user);
+
+        return "/login";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+
+    @GetMapping("/login-error")
+    @ResponseBody
+    public String getLoginError() {
+        return "The Username or Password that you have used is incorrect";
+    }
 }
